d8a1ed061b03d5467bbf6e68341a7067ed94d914
Marcel <marcelbastos12@hotmail.com>
2018-04-18 22:11:22 -0300

    início da codação

--- Jogodenovo/Form1.Designer.cs
+++ Jogodenovo/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Jogodenovo
 {
-    partial class Form1
+    partial class Tinicial
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btniniciar = new System.Windows.Forms.Button();
+            this.tBmostrar1 = new System.Windows.Forms.TextBox();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.tBmostrar2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btniniciar
+            // 
+            this.btniniciar.Location = new System.Drawing.Point(153, 177);
+            this.btniniciar.Name = "btniniciar";
+            this.btniniciar.Size = new System.Drawing.Size(105, 42);
+            this.btniniciar.TabIndex = 0;
+            this.btniniciar.Text = "Clique agora";
+            this.btniniciar.UseVisualStyleBackColor = true;
+            this.btniniciar.Click += new System.EventHandler(this.btniniciar_Click);
+            // 
+            // tBmostrar1
+            // 
+            this.tBmostrar1.Location = new System.Drawing.Point(85, 136);
+            this.tBmostrar1.Name = "tBmostrar1";
+            this.tBmostrar1.Size = new System.Drawing.Size(100, 20);
+            this.tBmostrar1.TabIndex = 1;
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(150, 271);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(0, 13);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Click += new System.EventHandler(this.lblNome_Click);
+            // 
+            // tBmostrar2
+            // 
+            this.tBmostrar2.Location = new System.Drawing.Point(233, 136);
+            this.tBmostrar2.Name = "tBmostrar2";
+            this.tBmostrar2.Size = new System.Drawing.Size(100, 20);
+            this.tBmostrar2.TabIndex = 3;
+            this.tBmostrar2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Tinicial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(404, 360);
+            this.Controls.Add(this.tBmostrar2);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.tBmostrar1);
+            this.Controls.Add(this.btniniciar);
+            this.Name = "Tinicial";
+            this.Text = "Tela inicial";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btniniciar;
+        private System.Windows.Forms.TextBox tBmostrar1;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox tBmostrar2;
     }
 }
 
--- Jogodenovo/Form1.cs
+++ Jogodenovo/Form1.cs
@@ -10,11 +10,29 @@ using System.Windows.Forms;
 
 namespace Jogodenovo
 {
-    public partial class Form1 : Form
+    public partial class Tinicial : Form
     {
-        public Form1()
+        public Tinicial()
         {
             InitializeComponent();
+        }
+
+        private void btniniciar_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            a = Convert.ToInt32(tBmostrar1.Text);
+            b = Convert.ToInt32(tBmostrar2.Text);
+            lblNome.Text = (a + b).ToString();
+        }
+
+        private void lblNome_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
